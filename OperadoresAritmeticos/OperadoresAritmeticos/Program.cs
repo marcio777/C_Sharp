8b04fd24aad9a93d226c0c8f52299e02c5bd3b08
marcio777 <marcio.infra@hotmail.com>
2017-12-09 00:12:53 -0200

    c#

---
+++ OperadoresAritmeticos/OperadoresAritmeticos/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperadoresAritmeticos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double num1, num2, soma;
+
+            Console.WriteLine("Digite o primeiro numero: "  );
+            num1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Digite o segundo numero: ");
+            num2 = Convert.ToDouble(Console.ReadLine());
+
+            soma = (num1 + num2);
+
+            Console.WriteLine("A soma e " + soma);
+
+
+
+           
+        }
+    }
+}
---
+++ Variaveis/Variaveis/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Variaveis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 20;
+            bool a = true;
+            int y;
+            y = x;
+            Console.WriteLine("Numero e  " + x.ToString());
+            Console.WriteLine("E o valor logico e  " + a.ToString());
+            Console.WriteLine("Numero e  " + y.ToString());
+
+        }
+    }
+}
